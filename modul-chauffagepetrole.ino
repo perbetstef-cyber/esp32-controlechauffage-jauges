ae9b7b4a020e26c43b36dfc8cddecbe99c63908b
perbetstef-cyber <perbetstef@gmail.com>
2026-05-22 13:58:49 +0200

    Update modul-chauffagepetrole.ino
    Reste a calibrer dans la fonction convertirADCVersTemp

--- modul-chauffagepetrole.ino
+++ modul-chauffagepetrole.ino
@@ -5,7 +5,7 @@
 #include <ArduinoJson.h>
 #include <HTTPClient.h>
 #include <ESPmDNS.h>
-
+#include <ArduinoOTA.h>
 
 WebServer server(80);
 ESPTelnet telnet;
@@ -13,7 +13,6 @@ ESPTelnet telnet;
 // --- CONFIGURATION PINS ---
 const int pinChauffe  = 27; // Sortie 1 (Chauffage liquide)
 const int pinTurbine  = 16; // Sortie 4 (Turbine PWM)
-const int pinSondeEau = 25; // Entrée sonde eau chaude
 
 // --- PARAMÈTRES PWM ---
 const int freq = 5000;
@@ -21,10 +20,40 @@ const int resolution = 8; // 0 à 255
 
 int SpinChauffe = 0;
 int SVitesseTurbine = 0; 
+const float vRef = 3.3;
 
 String ssid, pass;
-int dernierEtatEau = -1;
+String ota_pass = "dyqpa8we";
+int dernierEtatEauT = -1;
 unsigned long lastCheck = 0;
+
+// SONDE THERMIQUE //
+// --- CONFIGURATION PINS ---
+const int pinVoltageSonde = 35; // Nouvelle pin pour la sonde (GPIO 35)
+
+unsigned long dernierContactThermostat = 0; 
+const unsigned long DELAI_MAX_THERMOSTAT = 60000; // 1 minutes en millisecondes (60 * 1000)
+
+// --- TABLE DE CORRESPONDANCE (Tes relevés) ---
+float convertirADCVersTemp(float adc) {
+  if (adc < 200 || adc > 4000) return 00.0;
+
+  if (adc >= 1730) return 32.0;
+  if (adc >= 1615) return 35.0;
+  if (adc >= 1540) return 40.0;
+  if (adc >= 1480) return 45.0;
+  if (adc >= 1380) return 50.0;
+  if (adc >= 1300) return 55.0;
+  if (adc >= 1210) return 60.0;
+  if (adc >= 1125) return 65.0;
+  if (adc >= 1035) return 70.0;
+  if (adc >= 920)  return 75.0;
+  if (adc >= 850)  return 80.0;
+  if (adc >= 800)  return 85.0;
+  if (adc <= 750)  return 90.0;
+  return 25.0; // Par défaut
+}
+
 
 // --- PROTOTYPES ---
 void handleRoot();
@@ -36,6 +65,8 @@ void attemptConnection();
 void startBluetooth();
 void checkBTCommands();
 String trouverIpThermostat();
+
+
 
 void addToLog(String msg) {
   String entry = "[" + String(millis() / 1000) + "s] " + msg;
@@ -54,27 +85,45 @@ void setup() {
   // --- CONFIGURATION PWM (VERSION ESP32 3.0+) ---
   ledcAttach(pinTurbine, freq, resolution);
   ledcWrite(pinTurbine, 0); // Éteint au démarrage
+  appliquerVitesse(0);
 
-  pinMode(pinSondeEau, INPUT_PULLDOWN);
-  
-  
-  ssid = "CarsNet_Internal_Modul";
-  pass = "AdminCars123Esp32";
+  ssid = "CarsNet_Internal";
+  pass = "AdminCars123";
+
+  dernierContactThermostat = millis();
 
   attemptConnection();
 
 }
 
 void loop() {
-  if (WiFi.status() == WL_CONNECTED) {
-    server.handleClient();
-    telnet.loop();
-    if (millis() - lastCheck > 5000) {
+  ArduinoOTA.handle();
+  telnet.loop();
+  server.handleClient();
+
+  // --- SÉCURITÉ : VÉRIFICATION DU TIMEOUT THERMOSTAT ---
+  if (SpinChauffe == 1 && (millis() - dernierContactThermostat > DELAI_MAX_THERMOSTAT)) {
+    addToLog("⚠️ ALERTE : Pas de nouvelles du thermostat depuis 10 min ! Arrêt de sécurité.");
+    SpinChauffe = 0;
+    digitalWrite(pinChauffe, LOW);  // On coupe le chauffage
+    appliquerVitesse(0);            // On coupe la ventilation
+  }
+
+  if (WiFi.status() != WL_CONNECTED) {
+    static unsigned long lastReconnectAttempt = 0;
+    if (millis() - lastReconnectAttempt > 10000) { // Tentative toutes les 10s
+      addToLog("WiFi perdu, reconnexion...");
+      WiFi.reconnect();
+      lastReconnectAttempt = millis();
+    }
+    return; // On ne fait pas le reste si pas de WiFi
+  }
+
+  if (millis() - lastCheck > 5000) {
       recupererOrdres();
       surveillerEauChaude();
       lastCheck = millis();
     }
-  }
 }
 
 String trouverIpThermostat() {
@@ -85,8 +134,8 @@ void appliquerVitesse(int vitesse) {
   SVitesseTurbine = vitesse;
   int pwmVal = 0;
   
-  if (vitesse == 30)  pwmVal = 77;
-  else if (vitesse == 60)  pwmVal = 153;
+  if (vitesse == 30)  pwmVal = 155;
+  else if (vitesse == 60)  pwmVal = 202;
   else if (vitesse == 100) pwmVal = 255;
   else pwmVal = 0;
 
@@ -103,6 +152,7 @@ void recupererOrdres() {
   
   int httpCode = http.GET();
   if (httpCode == 200) {
+    dernierContactThermostat = millis();
     String payload = http.getString();
     JsonDocument doc;
     deserializeJson(doc, payload);
@@ -121,19 +171,29 @@ void recupererOrdres() {
 }
 
 void surveillerEauChaude() {
-  int etatActuel = (digitalRead(pinSondeEau) == LOW) ? 1 : 0;
-  if (etatActuel != dernierEtatEau) {
+  
+  long somme = 0;
+  for (int i = 0; i < 50; i++) { somme += analogRead(pinVoltageSonde); delay(2); }
+  float moyADC = (float)somme / 50.0;
+  addToLog("DEBUG ADC BRUT = " + String(moyADC));
+  int tempRelle = (int)convertirADCVersTemp(moyADC);
+  addToLog("Température Eau : " + String(tempRelle) + "°C");
+
+  if (tempRelle != dernierEtatEauT) {
     HTTPClient http;
     String ip = trouverIpThermostat();
     if (ip != "") {
-        String url = "http://"+ip+"/updatechauffagepetrole?mac=" + WiFi.macAddress() + "&val=" + String(etatActuel);
+        // On utilise le paramètre 'val' pour passer la température en degrés
+        String url = "http://"+ip+"/updatechauffagepetroletemperature?mac=" + WiFi.macAddress() + "&val=" + String(tempRelle);
         http.begin(url);
         if (http.GET() == 200) {
-          dernierEtatEau = etatActuel;
+          dernierEtatEauT = tempRelle;
         }
         http.end();
     }
   }
+
+  
 }
 
 void attemptConnection() {
@@ -144,17 +204,40 @@ void attemptConnection() {
     delay(500);
     Serial.print(".");
   }
-  if (WiFi.status() == WL_CONNECTED) {
-    MDNS.begin("modul-chauffagepetrole");
+  
+    if (MDNS.begin("modul-chauffagepetrole")) {
+    MDNS.addService("http", "tcp", 80);
+  }
     server.on("/", handleRoot);
     server.on("/relais1", handleR1);
     server.on("/turbine", handleTurbine);
     server.begin();
     telnet.begin();
+    telnet.onConnect([](String ip) {
+    addToLog("\nBienvenue sur le Modul-Chauffagepetrole !");
+  });
     addToLog("\nWiFi OK : " + WiFi.localIP().toString());
-  } else {
-    WiFi.disconnect();
-  }
+  
+  ArduinoOTA.setHostname("modul-chauffagepetrole");
+  ArduinoOTA.setPassword(ota_pass.c_str());
+
+  ArduinoOTA.onStart([]() {
+    addToLog("OTA START");
+  });
+
+  ArduinoOTA.onEnd([]() {
+    addToLog("OTA END");
+  });
+
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("OTA Progress: %u%%\r", (progress * 100) / total);
+  });
+
+  ArduinoOTA.onError([](ota_error_t error) {
+    addToLog("OTA ERROR : " + String((int)error));
+  });
+
+  ArduinoOTA.begin();
 }
 
 void handleR1() {
@@ -181,7 +264,11 @@ void handleRoot() {
   html += "<h1>🏠 MODUL-CHAUFFAGE (VARIATEUR)</h1>";
 
   html += "<div class='card'><h2>🔥 Eau Chaude (Sonde)</h2>";
-  html += (digitalRead(pinSondeEau) == LOW) ? "CHAUDE (Actif)" : "FROIDE (Inactif)";
+  long somme = 0;
+  for (int i = 0; i < 50; i++) { somme += analogRead(pinVoltageSonde); delay(2); }
+  float moyADC = (float)somme / 50.0;
+  int tempRelle = (int)convertirADCVersTemp(moyADC);
+  html += " température : "+String(tempRelle)+"°C";
   html += "</div>";
 
   html += "<div class='card'><h2>Chauffage Liquide (Pin 27)</h2>";
